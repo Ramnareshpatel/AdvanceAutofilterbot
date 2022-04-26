a8da49952d6cb62c78fcf25dcd904b80e417d62d
Ramnareshpatel <95334027+Ramnareshpatel@users.noreply.github.com>
2022-04-26 16:20:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,16 +34,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+AMHw_K1wvOM3MTU9'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🍂 ɢʀᴏᴜᴘ 🍂', url='https://t.me/Movies_4you'),
+            InlineKeyboardButton('🌿 ᴜᴘᴅᴀᴛᴇs 🌿', url='https://t.me/Movies4youBackup')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('CONTACT ME', url='https://t.me/Aadhi011'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('📚 ʜᴇʟᴘ 📚', callback_data='help'),
+            InlineKeyboardButton('🔱 ᴀʙᴏᴜᴛ 🔱', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('😎 ᴏᴡɴᴇʀ 😎', url='https://t.me/KingOf_univers')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -78,16 +77,15 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+AMHw_K1wvOM3MTU9'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('🍂 ɢʀᴏᴜᴘ 🍂', url='https://t.me/Movies_4you'),
+            InlineKeyboardButton('🌿 ᴜᴘᴅᴀᴛᴇs 🌿', url='https://t.me/Movies4youBackup')
             ],[      
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('CONTACT ME', url='https://t.me/Aadhi011'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('📚 ʜᴇʟᴘ 📚', callback_data='help'),
+            InlineKeyboardButton('🔱 ᴀʙᴏᴜᴛ 🔱', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('😎 ᴏᴡɴᴇʀ 😎', url='https://t.me/KingOf_univers')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
